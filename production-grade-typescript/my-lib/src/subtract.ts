b6f02c9849af6908b74889b426031cd050a54725
tatobig <tatobig@gmail.com>
2023-05-24 16:47:51 -0600

    Production Grade Typescript - Session 2

---
+++ production-grade-typescript/my-lib/src/subtract.ts
@@ -0,0 +1,12 @@
+/**
+ * Calculate the diference between two numbers
+ * 
+ * @param a - first number
+ * @param b - second numer
+ * 
+ * @beta
+ */
+
+export function subtract(a: number, b: number): number {
+  return a - b;
+}
